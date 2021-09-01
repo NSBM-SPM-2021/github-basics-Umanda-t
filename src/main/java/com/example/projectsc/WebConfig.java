af1815336cb113e1e5ff7058e455c0f4ed2643c2
Umanda-t <57774048+Umanda-t@users.noreply.github.com>
2021-09-01 14:08:34 +0530

    update_7 config

--- src/main/java/com/example/projectsc/WebConfig.java
+++ src/main/java/com/example/projectsc/WebConfig.java
@@ -1,10 +1,13 @@
 package com.example.projectsc;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
@@ -20,5 +23,15 @@ public class WebConfig extends WebMvcConfigurerAdapter {
                         "classpath:/static/css/",
                         "classpath:/static/fonts/",
                         "classpath:/static/js/");
+    }
+
+    @Bean
+    public ClassLoaderTemplateResolver webPageTemplateResolver(){
+        ClassLoaderTemplateResolver webPageTemplateResolver = new ClassLoaderTemplateResolver();
+        webPageTemplateResolver.setPrefix("templates/");
+        webPageTemplateResolver.setSuffix(".html");
+        webPageTemplateResolver.setTemplateMode("HTML5");
+        webPageTemplateResolver.setOrder(1);
+        return webPageTemplateResolver;
     }
 }
